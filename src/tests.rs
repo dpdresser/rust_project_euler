8c913f402c1e38af86cd269e415d9b57b4e4bcae
DANXPS\ddres <ddresser05@gmail.com>
2024-04-17 11:06:52 -0500

    Updated comments

--- src/tests.rs
+++ src/tests.rs
@@ -2,19 +2,21 @@
 mod tests {  
     use crate::functions::*;
 
+    // Problem 1
     #[test]
     fn test_multiples_of_3_or_5() {
-        assert_eq!(type_of(&multiples_of_3_or_5(10)), "u64"); // Returns u64
+        assert_eq!(type_of(&multiples_of_3_or_5(10)), "u64");
         assert_eq!(multiples_of_3_or_5(49), 543);
         assert_eq!(multiples_of_3_or_5(1000), 233168);
         assert_eq!(multiples_of_3_or_5(8456), 16687353);
         assert_eq!(multiples_of_3_or_5(19564), 89301183);
     }
 
+    // Problem 2
     #[test]
     fn test_fibonnaci_sum() {
-        assert_eq!(type_of(&fibonnaci_sum(10)), "u64"); // Returns u64
-        assert_eq!(fibonnaci_sum(10) % 2, 0); // Returns even number
+        assert_eq!(type_of(&fibonnaci_sum(10)), "u64");
+        assert_eq!(fibonnaci_sum(10) % 2, 0);
         assert_eq!(fibonnaci_sum(10), 10);
         assert_eq!(fibonnaci_sum(34), 44);
         assert_eq!(fibonnaci_sum(60), 44);
@@ -23,6 +25,7 @@ mod tests {
         assert_eq!(fibonnaci_sum(4000000), 4613732);
     }
 
+    // Problem 3
     #[test]
     fn test_largest_prime_factor() {
         let primes = prime_sieve_sundaram(7000);
